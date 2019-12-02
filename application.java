77bd5a5f77453ac6ad98f916c9d17f5bbe68cf5e
ruksanams <58158562+ruksanams@users.noreply.github.com>
2019-12-02 10:45:43 +0530

    swings

--- application.java
+++ application.java
@@ -1,10 +1,9 @@
-package swing;
-
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import java.awt.BorderLayout;
+import javax.swing.JCheckBox;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
@@ -13,7 +12,8 @@ import java.awt.event.ActionEvent;
 public class application {
 
 	private JFrame frame;
-	private JTextField textField;
+	private JTextField t2;
+	private JTextField t1;
 
 	/**
 	 * Launch the application.
@@ -43,54 +43,50 @@ public class application {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 453, 299);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JLabel lblUserName = new JLabel("Degree");
-		lblUserName.setBounds(41, 90, 120, 20);
-		frame.getContentPane().add(lblUserName);
+		JLabel l1 = new JLabel("user name");
+		l1.setBounds(29, 16, 95, 46);
+		frame.getContentPane().add(l1);
 		
-		textField = new JTextField();
-		textField.setBounds(209, 87, 142, 26);
-		frame.getContentPane().add(textField);
-		textField.setColumns(10);
-		JLabel label = new JLabel("");
-		label.setBounds(220, 115, 131, 20);
-		frame.getContentPane().add(label);
+		JLabel l2 = new JLabel("password");
+		l2.setBounds(29, 107, 95, 20);
+		frame.getContentPane().add(l2);
 		
-		JButton btnToCelcious = new JButton("To celcious");
-		btnToCelcious.addActionListener(new ActionListener()
-		{
-			
+		t2 = new JTextField();
+		t2.setBounds(168, 26, 146, 26);
+		frame.getContentPane().add(t2);
+		t2.setColumns(10);
+		
+		t1 = new JTextField();
+		t1.setBounds(168, 105, 146, 26);
+		frame.getContentPane().add(t1);
+		t1.setColumns(10);
+		JLabel L3 = new JLabel("");
+		JButton b1 = new JButton("SUBMIT");
+		b1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) 
-			{
+	{
+			if(t1.getText().equals("ruksana")&& t2.getText().equals("123"))
+			{	
+				L3.setText("SUCCESS");
+			}
 				
-				float t=Float.parseFloat(textField.getText());
-				float temp=(float)(t-273.15);
-				String str=String.valueOf(temp);
-				label.setText("Result="+str);
+			else
+			{	
+				L3.setText("UN SUCCESS");
+			}
+					
+					
 			}
 		});
-		btnToCelcious.setBounds(15, 178, 115, 29);
-		frame.getContentPane().add(btnToCelcious);
+		b1.setBounds(118, 160, 115, 29);
+		frame.getContentPane().add(b1);
 		
-		
-		JButton btnToFahrenheit = new JButton("To Fahrenheit");
-		btnToFahrenheit.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				float t=Float.parseFloat(textField.getText());
-				float temp=(float)(t-273.15)*9/5+32;
-				String str=String.valueOf(temp);
-				label.setText("Result="+str);
-			}
-		});
-		btnToFahrenheit.setBounds(214, 178, 137, 29);
-		frame.getContentPane().add(btnToFahrenheit);
-		
-		JLabel lblNewLabel = new JLabel("");
-		lblNewLabel.setBounds(124, 129, 120, 33);
-		frame.getContentPane().add(lblNewLabel);
-		
-		}
+		//JLabel L3 = new JLabel("");
+		L3.setBounds(86, 205, 228, 20);
+		frame.getContentPane().add(L3);
+	}
 }
